1074ac2c9c36c14eeae1bd6cc5dc6dc274fdf084
Francesca Michel <francescamichel@gmail.com>
2015-08-03 10:26:45 -0400

    PinkSofa

---
+++ main.js
@@ -0,0 +1,67 @@
+// alert("hello world!");
+
+// function change(){
+// 	.document.getElementById('stars').innerHTML = "NEW TEXT";
+
+// }
+
+// <div onclick="change()" id="stars"> CHANGE ME </div>
+
+
+
+// when we put something inside of quotes, we are telling javascript that it is a string 
+// all numbers in JS are floats
+//boolean- true, false
+//undefined (no value)
+
+// variables: empty containers for values
+// to create a variable in java: 
+// var name = "Zach";
+// var numberOfWidgets = 10;
+// var isCodingCool = true; 
+// if you assign a variable a value, and then retype the variable, JS will tell you the value that you assigned
+// arrays are used to hold a collection of data, of any data type
+// this one is full of strings: 
+// ["snoopy", "charlie brown", "patty", "violet"]
+// snoopy would be value 0, charlie brown would be value 1, patty value 2, etc. 
+// 
+// you might see the array in a contect like this; 
+// var class_names = ["snoopy", "charlie brown", "patty", "violet"]
+
+var num1 = "love";
+var num2 = " conquers all";
+
+
+
+
+var myInfo = {firstName:'Francesca', lastName:'Michel', birthday:'mayeighteenthnineteenninetythree'};
+var myArray = ["i", "love", "kurtcobain"]
+
+console.log(myInfo.firstName);
+
+console.log(myInfo.lastName);
+
+console.log(myInfo.birthday);
+
+console.log(myArray[0]); 
+console.log(myArray[1]); 
+console.log(myArray[2]); 
+
+// var myNum = 20;
+
+// if(myNum < 10){
+// 	alert("Number is less than 10!");
+// }
+// else{
+
+// }
+
+function yourName(theName){
+	alert(theName);
+}
+
+yourName("Bob");
+
+
+
+
